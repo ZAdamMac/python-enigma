ee2920baad482c2da18f98d045f114b1916604c3
zadammac <zachary.j.adam@gmail.com>
2018-11-05 10:05:47 -0400

    Settings correction.

--- python_enigma/helloworld.py
+++ python_enigma/helloworld.py
@@ -7,12 +7,12 @@ with open("catalogue.json", "r") as file:
     all_wheels = json.load(file)
 
 use_these = [("I", "A"), ("II", "B"), ("III", "C")]
-machine = enigma.Enigma(catalog="default", stecker="QZ",
+machine = enigma.Enigma(catalog="default", stecker="AQ BJ",
                         rotors=use_these, reflector="Reflector B", operator=True, word_length=5, stator="military")
 machine.set_wheels("ABC")
 
 print("I am going to try to print HELLO WORLD using the stecker settings\n"
-      "AQ and BJ and rotors I, II, III from the whermact set.\n"
+      "AQ and BJ and rotors I, II, III from the whermacht set.\n"
       "The ringstellungs are A, B, and C respectively.")
 crypted = machine.parse("hello world")
 print(crypted)
