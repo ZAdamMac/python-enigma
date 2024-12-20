7f551d4336074ba618cd2bdb6de245497676b6af
Jeffrey Goldberg <jeffrey@goldmark.org>
2024-12-19 20:00:16 -0600

    test that wheels advance

--- tests/test_cryption.py
+++ tests/test_cryption.py
@@ -1,4 +1,5 @@
 import sys
+from collections import Counter
 import pytest
 
 from python_enigma import enigma
@@ -59,6 +60,38 @@ class TestHistorical:
 
         decrypted = machine.parse(ctext)
         assert decrypted == ptext
+
+    def test_greek_advance(self) -> None:
+        wheels: list[str] = ["Beta", "V", "VI", "VIII"]
+        rotor_positions = "CDSZ"
+        ring_settings = "EPEL"
+        reflector = "Reflector C Thin"
+        plug_board = "AE BF CM DQ HU JN LX PR SZ VW"
+        rotors = list(zip(wheels, list(rotor_positions)))
+
+        ptext = "AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA"
+
+        machine = enigma.Enigma(
+            catalog="default",
+            stecker=plug_board,
+            stator="military",
+            rotors=rotors,
+            reflector=reflector,
+            operator=False,
+        )
+        machine.set_wheels(ring_settings)
+
+        (p_most_common, p_most_commen_count) = Counter(ptext).most_common(1)[0]
+
+        # just checking the counter logic
+        assert p_most_common == "A"
+        assert p_most_commen_count == len(ptext)
+
+        ctext = machine.parse(ptext)
+        (_, c_most_commen_count) = Counter(ctext).most_common(1)[0]
+
+        # This test will fail if rotors aren't advancing
+        assert c_most_commen_count != len(ctext)
 
 
 if __name__ == "__main__":
