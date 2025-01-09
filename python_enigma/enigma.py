3f24ad8b007a2cea9773b50bf3048f2f1dad3a58
Jeffrey Goldberg <jeffrey@goldmark.org>
2025-01-09 15:24:46 -0600

    fmt

--- python_enigma/enigma.py
+++ python_enigma/enigma.py
@@ -458,99 +458,36 @@ ignore_static_wheels={self.ignore_static_wheels})"""
 def alpha_to_index(char):
     """Takes a single character and converts it to a number where A=0"""
     translator = {
-        "A": 0,
-        "B": 1,
-        "C": 2,
-        "D": 3,
-        "E": 4,
-        "F": 5,
-        "G": 6,
-        "H": 7,
-        "I": 8,
-        "J": 9,
-        "K": 10,
-        "L": 11,
-        "M": 12,
-        "N": 13,
-        "O": 14,
-        "P": 15,
-        "Q": 16,
-        "R": 17,
-        "S": 18,
-        "T": 19,
-        "U": 20,
-        "V": 21,
-        "W": 22,
-        "X": 23,
-        "Y": 24,
-        "Z": 25,
-    }
+        "A": 0, "B": 1, "C": 2, "D": 3, "E": 4, "F": 5,
+        "G": 6, "H": 7, "I": 8, "J": 9, "K": 10,
+        "L": 11, "M": 12, "N": 13, "O": 14, "P": 15,
+        "Q": 16, "R": 17, "S": 18, "T": 19, "U": 20,
+        "V": 21, "W": 22, "X": 23, "Y": 24, "Z": 25,
+    }  # fmt: skip
     return translator[char.upper()]
 
 
 def alpha_to_num(char):
     """Takes a single character and converts it to a number where A=1"""
     translator = {
-        "A": 1,
-        "B": 2,
-        "C": 3,
-        "D": 4,
-        "E": 5,
-        "F": 6,
-        "G": 7,
-        "H": 8,
-        "I": 9,
-        "J": 10,
-        "K": 11,
-        "L": 12,
-        "M": 13,
-        "N": 14,
-        "O": 15,
-        "P": 16,
-        "Q": 17,
-        "R": 18,
-        "S": 19,
-        "T": 20,
-        "U": 21,
-        "V": 22,
-        "W": 23,
-        "X": 24,
-        "Y": 25,
-        "Z": 26,
-    }
+        "A": 1, "B": 2, "C": 3, "D": 4, "E": 5, "F": 6,
+        "G": 7, "H": 8, "I": 9, "J": 10, "K": 11, "L": 12,
+        "M": 13, "N": 14, "O": 15, "P": 16, "Q": 17, "R": 18,
+        "S": 19, "T": 20, "U": 21, "V": 22, "W": 23, "X": 24,
+        "Y": 25, "Z": 26,
+    }  # fmt: skip
     return translator[char.upper()]
 
 
 def num_to_alpha(integ):
     """takes a numeral value and spits out the corresponding letter."""
     translator = {
-        1: "A",
-        2: "B",
-        3: "C",
-        4: "D",
-        5: "E",
-        6: "F",
-        7: "G",
-        8: "H",
-        9: "I",
-        10: "J",
-        11: "K",
-        12: "L",
-        13: "M",
-        14: "N",
-        15: "O",
-        16: "P",
-        17: "Q",
-        18: "R",
-        19: "S",
-        20: "T",
-        21: "U",
-        22: "V",
-        23: "W",
-        24: "X",
-        25: "Y",
-        26: "Z",
-    }
+        1: "A", 2: "B", 3: "C", 4: "D", 5: "E", 6: "F",
+        7: "G", 8: "H", 9: "I", 10: "J", 11: "K", 12: "L",
+        13: "M", 14: "N", 15: "O", 16: "P", 17: "Q", 18: "R",
+        19: "S", 20: "T", 21: "U", 22: "V", 23: "W", 24: "X",
+        25: "Y", 26: "Z",
+    }  # fmt: skip
     return translator[integ]
 
 
